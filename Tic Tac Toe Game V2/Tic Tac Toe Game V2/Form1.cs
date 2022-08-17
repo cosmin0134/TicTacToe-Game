9dc27ec0a66f01b55342bb5f18bcf5dd2103a4fa
Nedelcu Cosmin <103001090+cosmin0134@users.noreply.github.com>
2022-08-18 00:33:06 +0300

    Add files via upload

---
+++ Tic Tac Toe Game V2/Tic Tac Toe Game V2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game_V2
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; //true = X turn; false = O turn;
+        int turn_count = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Game created by Cosmin", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+        }
+
+        private void checkForWinner()
+        {
+            bool there_is_a_winner = false;
+
+            //horizontal winner
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            //vertical winner
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            //diagonal winner
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+         
+            if (there_is_a_winner)
+            {
+                disableButtons();
+
+                String winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+
+                MessageBox.Show(winner + " Wins!", "Yay");
+            }//end if
+            else
+            {
+                if (turn_count == 9)
+                    MessageBox.Show("Its a draw", "Bummer!"); 
+            }
+
+        }//end of checkForWinner
+
+        private void disableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                    {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                    }//end foreach
+            }
+            catch { };
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }//end foreach
+            }
+            catch { };
+        }
+    }
+}
